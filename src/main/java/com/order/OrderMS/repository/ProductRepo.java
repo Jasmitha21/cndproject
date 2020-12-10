f7d3a0d91fda562daf319c30138bdb4228d4b389
Jasmitha21 <75603206+Jasmitha21@users.noreply.github.com>
2020-12-10 16:00:42 +0530

    Add files via upload

---
+++ src/main/java/com/order/OrderMS/repository/ProductRepo.java
@@ -0,0 +1,11 @@
+package com.order.OrderMS.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.order.OrderMS.entity.ProductsEntity;
+
+@Repository
+public interface ProductRepo  extends JpaRepository<ProductsEntity, Integer> {
+	
+
+}
